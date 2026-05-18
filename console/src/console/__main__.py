a8a6f41a2e62b349df9899c3bea1dcb980f472ca
Mohamed-Eltahhan <mohamed.eltahhan1@gmail.com>
2026-05-18 22:16:28 +0300

    more setup

--- console/src/console/__main__.py
+++ console/src/console/__main__.py
@@ -1,8 +1,11 @@
 from PySide6.QtWidgets import QApplication
-from console.gui.main_window import MainWindow
+from gui.main_window import MainWindow
 
 def main():
     app = QApplication()
     main_window = MainWindow()
     main_window.show()
     app.exec()
+
+if __name__ == "__main__":
+    main()
