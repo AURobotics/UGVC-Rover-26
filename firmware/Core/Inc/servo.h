998ad85779dfd51a38ef43374aab0b291b269515
unknown <youssramhfarghaly@gmail.com>
2026-05-21 19:15:18 +0300

    fixed servo

--- firmware/Core/Inc/servo.h
+++ firmware/Core/Inc/servo.h
@@ -1,15 +1,16 @@
 #pragma once
-// #include "servo.h"
 #include <cstdint>
+#include "PWM_expander.h"
 
 class Servo {
 public:
-    Servo(PCA9685& pca, uint8_t channel);
-    float setAngle(float angle);
+    Servo(PWM_expander& pca, PWM_channel channel);
+    void setAngle(float angle);
     float getAngle();
 private:
-    PCA9685& pca;// esmo pwm_expander check pwm_expander.cpp in roder to use the functions in it
-    uint8_t channel;
-    float current_angle;
-    const float pulse_range[2] = {1000, 2000}; // in microseconds NEED TO CHECK THESE ACCORDING TO OUR SERVO not sure which model yet
+    PWM_expander& pca;
+    const PWM_channel   channel;
+    float current_angle = 0.0f;
+    static constexpr float period_us = 20000.0f; // 20ms period
+    static constexpr float pulse_range[2] = {1000.0f, 2000.0f}; // in microseconds NEED TO CHECK THESE ACCORDING TO OUR SERVO not sure which model yet
 };
