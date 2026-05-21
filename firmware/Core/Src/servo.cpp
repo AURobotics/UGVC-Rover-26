9668bdb3665b9a09acf63d87fa279f07f7fc97ff
unknown <youssramhfarghaly@gmail.com>
2026-05-21 19:15:18 +0300

    fixed servo

--- firmware/Core/Src/servo.cpp
+++ firmware/Core/Src/servo.cpp
@@ -1,6 +1,6 @@
 #include "servo.h"
 
-Servo::Servo(PCA9685& pca, uint8_t channel) : pca(pca), channel(channel) {
+Servo::Servo(PWM_expander& pca, PWM_channel channel) : pca(pca), channel(channel) {
     setAngle(0);
 }
 
@@ -10,9 +10,10 @@ void Servo::setAngle(float angle) {
     else if (angle > 180) angle = 180.0f;
 
     float pulse_width = pulse_range[0] + angle * (pulse_range[1] - pulse_range[0]) / 180.0f; // mapping from angle to pulse_width
-    uint16_t count = (pulse_width / pulse_range[1] * 4096.0f); // Convert pulse_width to count (0-4095)
+    uint16_t count = (pulse_width / period_us * 4096.0f); // Convert pulse_width to count (0-4095)
 
-    pca.setChannel(channel, 0, count);  // Motawe3's function
+    pca.set_channel(channel, count); 
+    pca.write();
     current_angle = angle;
 }
 
