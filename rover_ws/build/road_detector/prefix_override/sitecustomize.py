ab66cffc3c1af7302d6cc1dd540fde34fe1dfee4
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-05-22 19:01:16 +0300

    removed ros2 build artifacts

--- rover_ws/build/road_detector/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == 'D:\\My_Files\\Work\\AU-Robotics\\Rover_2026\\Code\\UGVC-Rover-26\\rover_ws\\.pixi\\envs\\default':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = 'D:\\My_Files\\Work\\AU-Robotics\\Rover_2026\\Code\\UGVC-Rover-26\\rover_ws\\install\\road_detector'
