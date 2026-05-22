b192c5d21f3d080b6a52ad8e1aa5240e67517f38
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-05-22 19:01:16 +0300

    removed ros2 build artifacts

--- rover_ws/build/road_detector/share/road_detector/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\D:\My_Files\Work\AU-Robotics\Rover_2026\Code\UGVC-Rover-26\rover_ws\build\road_detector"
