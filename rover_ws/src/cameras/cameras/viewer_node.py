6bbe1e93a2eb2751e66e385c836b1a93396b19f7
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2026-05-26 02:52:25 +0300

    corrected window override

--- rover_ws/src/cameras/cameras/viewer_node.py
+++ rover_ws/src/cameras/cameras/viewer_node.py
@@ -18,18 +18,27 @@ class Viewer(Node):
             depth=1
             )
         
-        self.create_subscription(Image, '/camera1/image_raw', self.camera_cb, image_qos)
-        self.create_subscription(Image, '/camera2/image_raw', self.camera_cb, image_qos)
+        self.create_subscription(Image, '/camera1/image_raw', self.camera1, image_qos)
+        self.create_subscription(Image, '/camera2/image_raw', self.camera2, image_qos)
         
         self.get_logger().info('Viewer initialized with 2 windows')
 
-    def camera_cb(self, msg):
+    def camera1(self, msg):
         try:
             frame = self.bridge.imgmsg_to_cv2(msg, 'passthrough')
-            cv2.imshow('Camera Feed', frame)
+            cv2.imshow('Camera 1 Feed', frame)
             cv2.waitKey(1)
         except Exception as e:
-            self.get_logger().error(f'Camera error: {e}')
+            self.get_logger().error(f'Camera 1 error: {e}')
+
+    def camera2(self, msg):
+        try:
+            frame = self.bridge.imgmsg_to_cv2(msg, 'passthrough')
+            cv2.imshow('Camera 2 Feed', frame)
+            cv2.waitKey(1)
+        except Exception as e:
+            self.get_logger().error(f'Camera 2 error: {e}')
+
 
 def main(args=None):
     rclpy.init(args=args)
