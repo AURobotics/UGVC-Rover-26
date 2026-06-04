1c0a8c0d7b1f81bd6e380168114297d557d9f0e7
Youssef Samy <office.youssefkassem@gmail.com>
2026-06-04 20:23:22 +0000

    Joystick integration test general fixes & dpad addition

--- console/src/console/gui/main_window.py
+++ console/src/console/gui/main_window.py
@@ -1,7 +1,10 @@
 from PySide6.QtWidgets import QMainWindow
 
+from console.ros_nodes.worker import ROS2Worker
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("UGVC Rover 26 Console")
-        ...
+        self.ros2 = ROS2Worker(self)
+        self.ros2.start()
