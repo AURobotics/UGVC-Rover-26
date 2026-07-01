47aca9d079450dc4e14b56d26809c973f60361f0
Abdelaziz-Islam-Galal <abdelaziz.islam.galal@gmail.com>
2026-07-02 01:33:34 +0300

    updated mission launch file to current merged pkgs

--- rover_ws/src/mission/launch/mission.launch.py
+++ rover_ws/src/mission/launch/mission.launch.py
@@ -1,10 +1,26 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    cameras_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([FindPackageShare('cameras'), 'launch', 'launch.py'])
+        ])
+    )
+    #IMPORTANT:
+    #       I am depending on args in the caneras launch file
+    #       should I pass them here? 
+
+    face_recognition_node = Node(
+            package='face_recognition',
+            executable='face_recognition_node',
+            name='face_recognition_node',
+    )
+    
     declare_params_file = DeclareLaunchArgument(
         'params_file',
         default_value=PathJoinSubstitution([
@@ -26,6 +42,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        cameras_launch,
+        face_recognition_node,
         declare_params_file,
-        mission_node
+        mission_node,
     ])
