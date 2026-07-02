7ea45c11110c73df85c3f90e777b2c1aa9822171
Mohamed-Eltahhan <mohamed.eltahhan1@gmail.com>
2026-07-03 02:32:13 +0300

    Added camera selection + code restructuring

--- console/src/console/gui/main_tab.py
+++ console/src/console/gui/main_tab.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QMainWindow, QDockWidget
-from PySide6.QtCore import Qt, QTimer
+from PySide6.QtCore import Qt
 from console.gui.camera_display import CameraDisplay
 
 class MainTab(QMainWindow):
@@ -17,14 +17,9 @@ class MainTab(QMainWindow):
             | QMainWindow.DockOption.VerticalTabs
         )
 
-        self._cam = CameraDisplay()
-        self._cam_timer = QTimer()
-        self._cam_timer.timeout.connect(self.update_camera_display)
-        self._cam_timer.setInterval(33)  # Approximately 30 FPS
-        self._cam_timer.start()
+        self._cam = CameraDisplay(self.mediator, self)
         self._cam_dock = self._create_dock("Camera", self._cam)
         self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, self._cam_dock)
-
 
     def _create_dock(self, title, widget):
         dock = QDockWidget(title, self)
@@ -35,21 +30,14 @@ class MainTab(QMainWindow):
         )
         return dock
     
-    def update_camera_display(self):
-        frame = self.mediator.get_video_frame()
-        if frame is not None:
-            self._cam.update_frame(frame)
-    
     def hideEvent(self, event):
         super().hideEvent(event)
         for dock in self.findChildren(QDockWidget):
             if dock.isFloating():
                 dock.hide()
-        self._cam_timer.stop()
 
     def showEvent(self, event):
         super().showEvent(event)
         for dock in self.findChildren(QDockWidget):
             if dock.isFloating():
                 dock.show()
-        self._cam_timer.start()
