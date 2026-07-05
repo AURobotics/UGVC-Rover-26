d1fcac8b8be2490b194fff7545d3301bdc47a5a0
Mohamed-Eltahhan <mohamed.eltahhan1@gmail.com>
2026-07-05 21:23:04 +0300

    Speedomemter and battery widgets

--- console/src/console/gui/main_tab.py
+++ console/src/console/gui/main_tab.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QMainWindow, QDockWidget
 from PySide6.QtCore import Qt
 from console.gui.camera_display import CameraDisplay
+from console.gui.status_widget import StatusWidget
 
 class MainTab(QMainWindow):
     def __init__(self, mediator, parent=None):
@@ -20,6 +21,14 @@ class MainTab(QMainWindow):
         self._cam = CameraDisplay(self.mediator, self)
         self._cam_dock = self._create_dock("Camera", self._cam)
         self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, self._cam_dock)
+
+        self._battery_status = StatusWidget("batteries.qml", self.mediator, self)
+        self._battery_dock = self._create_dock("Battery Status", self._battery_status)
+        self.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, self._battery_dock)
+
+        self._speedometer = StatusWidget("speedometer.qml", self.mediator, self)
+        self._speedometer_dock = self._create_dock("Speedometer", self._speedometer)
+        self.splitDockWidget(self._battery_dock, self._speedometer_dock, Qt.Orientation.Horizontal)
 
     def _create_dock(self, title, widget):
         dock = QDockWidget(title, self)
