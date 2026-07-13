db7e2afe22d145f7b2bdee42f126b17b500762ec
Abdelaziz-Islam-Galal <abdelaziz.islam.galal@gmail.com>
2026-07-14 01:39:20 +0300

    auto led

--- rover_ws/src/qualify/qualify/auto_led.py
+++ rover_ws/src/qualify/qualify/auto_led.py
@@ -1,8 +1,10 @@
 import rclpy
 from rclpy.node import Node
 from std_srvs.srv import SetBool
+from std_msgs.msg import Bool
 
 MANUAL_TOGGLE_TOPIC = '/manual_toggle'
+STM_LED_TOPIC = '/rover/mode'
 
 
 class AutoLed(Node):
@@ -12,8 +14,16 @@ class AutoLed(Node):
                         SetBool,
                         MANUAL_TOGGLE_TOPIC,
                         self.manual_toggle_callback
-                    )        
+                    )     
+        self.stm_led_publisher = self.create_publisher(
+            Bool,
+            STM_LED_TOPIC,
+            10
+        ) 
         self.auto_led_state = False
+        self.create_timer(0.1, self.publish_led_state)  
+
+        # self.test_subscriber()
 
     def manual_toggle_callback(self, request, response):
         self.set_led(request.data)
@@ -27,6 +37,21 @@ class AutoLed(Node):
         else:
             self.get_logger().info("Turning on auto LED")
             self.auto_led_state = True
+
+    def publish_led_state(self):
+        msg = Bool()
+        msg.data = self.auto_led_state
+        self.stm_led_publisher.publish(msg)
+
+    # def test_subscriber(self):
+    #     self.create_subscription(
+    #         Bool,
+    #         STM_LED_TOPIC,
+    #         self.led_callback,
+    #         10
+    #     )
+    # def led_callback(self, msg):
+    #     self.get_logger().info(f"led is: {msg.data}")
 
 
 def main(args=None):
